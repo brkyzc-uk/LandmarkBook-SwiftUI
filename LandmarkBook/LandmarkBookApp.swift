950ebc3a3b6559b3923020e44b82b83160e6d443
brkyzc-uk <burak.yazici@anadolu.edu.tr>
2022-05-11 10:35:25 +0300

    Initial Commit

---
+++ LandmarkBook/LandmarkBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkBookApp.swift
+//  LandmarkBook
+//
+//  Created by Burak YAZICI on 11/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
